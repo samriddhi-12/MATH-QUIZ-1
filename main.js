1fbea001bd92643f4a0cb62636e0cda100bd279a
samriddhi-12 <85345001+samriddhi-12@users.noreply.github.com>
2021-08-14 15:11:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+player1_name = localStorage.getItem("player1_name");
+player2_name = localStorage.getItem("player2_name");
+
+player1_score = 0;
+player2_score = 0;
+
+function login() {
+    window.location="project90.html";
+}
